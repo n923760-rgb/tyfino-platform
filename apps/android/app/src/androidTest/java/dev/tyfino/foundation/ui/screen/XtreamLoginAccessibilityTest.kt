b66591510b32a291fdced4172f4f1f1c1cfe50d3
n923760-rgb <n923760@gmail.com>
2026-09-23 21:40:19 +0300

    Fix Android instrumentation test import

--- apps/android/app/src/androidTest/java/dev/tyfino/foundation/ui/screen/XtreamLoginAccessibilityTest.kt
+++ apps/android/app/src/androidTest/java/dev/tyfino/foundation/ui/screen/XtreamLoginAccessibilityTest.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.semantics.LiveRegionMode
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
