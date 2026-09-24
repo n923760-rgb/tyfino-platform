e45a01d5c427f52e853690ca31b21c0dfe326075
n923760-rgb <n923760@gmail.com>
2026-09-24 18:30:35 +0300

    android: import width for catalog sidebar

--- apps/android/app/src/main/java/dev/tyfino/foundation/ui/screen/CatalogScreen.kt
+++ apps/android/app/src/main/java/dev/tyfino/foundation/ui/screen/CatalogScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyColumn
